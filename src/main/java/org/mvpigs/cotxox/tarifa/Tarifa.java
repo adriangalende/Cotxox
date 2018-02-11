fc421dc797c5417e3476aa5c98bb36eaf9520048
Adrián <adriangalende@gmail.com>
2018-02-11 18:19:44 +0100

    get coste tiempo

--- src/main/java/org/mvpigs/cotxox/tarifa/Tarifa.java
+++ src/main/java/org/mvpigs/cotxox/tarifa/Tarifa.java
@@ -2,9 +2,14 @@ package org.mvpigs.cotxox.tarifa;
 
 public class Tarifa {
     private final double  COSTE_MILLA = 3.15;
+    private final double COSTE_TIEMPO = 0.35;
 
 
     public double getCosteDistancia(double distancia) {
         return distancia * COSTE_MILLA;
+    }
+
+    public double getCosteTiempo(int minutos) {
+        return minutos * COSTE_TIEMPO;
     }
 }
--- src/test/java/org/mvpigs/cotxox/TarifaTest.java
+++ src/test/java/org/mvpigs/cotxox/TarifaTest.java
@@ -7,12 +7,23 @@ import static org.junit.Assert.assertEquals;
 public class TarifaTest() {
     @Test
     public void testGetCosteDistancia() {
-        
+
         Tarifa tarifa = new Tarifa();
 
         double distancia = 7.75;
         double costeMilla = 1.35;
 
        assertEquals( (distancia * costeMilla) , tarifa.getCosteDistancia(distancia));
+    }
+
+    @Test
+    public void testGetCosteTiempo() {
+        Tarifa tarifa = new Tarifa();
+
+        int tiempoEsperadoMinutos = 10;
+        double costeMinuto = 0.35; 
+        
+        assertEquals((tiempoEsperadoMinutos * costeMinuto), tarifa.getCosteTiempo(tiempoEsperadoMinutos));
+
     }
 }
