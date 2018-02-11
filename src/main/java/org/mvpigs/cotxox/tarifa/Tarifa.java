7f9c168860389b245989215ec1bfed3ba1248cb9
Adrián <adriangalende@gmail.com>
2018-02-11 18:51:37 +0100

    get coste total estimado

--- src/main/java/org/mvpigs/cotxox/tarifa/Tarifa.java
+++ src/main/java/org/mvpigs/cotxox/tarifa/Tarifa.java
@@ -1,8 +1,12 @@
 package org.mvpigs.cotxox.tarifa;
 
+import org.mvpigs.cotxox.carrera.*;
+
 public class Tarifa {
-    private final double  COSTE_MILLA = 3.15;
+    private final double  COSTE_MILLA = 1.35;
     private final double COSTE_TIEMPO = 0.35;
+    public final double COSTE_MINIMO = 5;
+    public final double COSTE_COMISION = 1.2; // 20% de comision 
 
 
     public double getCosteDistancia(double distancia) {
@@ -11,5 +15,16 @@ public class Tarifa {
 
     public double getCosteTiempo(int minutos) {
         return minutos * COSTE_TIEMPO;
+    }
+
+    public double getCosteTotalEsperado(Carrera carrera) {
+        double costeTotalEsperado = (getCosteTiempo(carrera.getTiempoEsperado()) + 
+                getCosteDistancia(carrera.getDistancia())) * COSTE_COMISION;
+
+        if (costeTotalEsperado <= COSTE_MINIMO) {
+            return COSTE_MINIMO;
+        }
+
+        return costeTotalEsperado;
     }
 }
--- src/test/java/org/mvpigs/cotxox/TarifaTest.java
+++ src/test/java/org/mvpigs/cotxox/TarifaTest.java
@@ -1,10 +1,13 @@
 package org.mvpigs.cotxox;
 
 import org.mvpigs.cotxox.tarifa.*;
+import org.mvpigs.cotxox.carrera.*;
+
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-public class TarifaTest() {
+public class TarifaTest {
+
     @Test
     public void testGetCosteDistancia() {
 
@@ -12,18 +15,33 @@ public class TarifaTest() {
 
         double distancia = 7.75;
         double costeMilla = 1.35;
-
-       assertEquals( (distancia * costeMilla) , tarifa.getCosteDistancia(distancia));
+        System.out.println(tarifa.getCosteDistancia(distancia));
+        assertEquals((distancia * costeMilla), tarifa.getCosteDistancia(distancia), 0.01);
     }
 
     @Test
     public void testGetCosteTiempo() {
         Tarifa tarifa = new Tarifa();
-
         int tiempoEsperadoMinutos = 10;
-        double costeMinuto = 0.35; 
-        
-        assertEquals((tiempoEsperadoMinutos * costeMinuto), tarifa.getCosteTiempo(tiempoEsperadoMinutos));
+        double costeMinuto = 0.35;
+        System.out.println(tarifa.getCosteTiempo(tiempoEsperadoMinutos));
+        assertEquals((tiempoEsperadoMinutos * costeMinuto), tarifa.getCosteTiempo(tiempoEsperadoMinutos), 0.01);
 
+    }
+
+    @Test
+    public void testGetCosteTotalEsperado() {
+        String tarjetaCredito = "4916119711304546";
+        double distancia = 7.75;
+        int tiempoEsperadoMinutos = 10;
+
+        Carrera carrera = new Carrera(tarjetaCredito);
+        carrera.setTiempoEsperado(tiempoEsperadoMinutos);
+        carrera.setDistancia(distancia);
+
+        Tarifa tarifa = new Tarifa();
+        tarifa.getCosteTotalEsperado(carrera);
+        System.out.println(tarifa.getCosteTotalEsperado(carrera));
+        assertEquals(16.755, tarifa.getCosteTotalEsperado(carrera), 0.01);
     }
 }
